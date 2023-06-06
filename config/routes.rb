296e713dc8eabd52411753d0dd1846b79572911d
Emilia Zysk <emzysk.dev@gmail.com>
2023-06-06 18:51:24 +0900

    rails generate controller GamesController new score

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  get 'games/new'
+  get 'games/score'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-
-  get "ask", to: "questions#ask", as: :ask
-  get "answer", to: "questions#answer", as: :answer
 end
